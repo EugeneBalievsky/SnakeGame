4275b8de51867720aadce23ed05e9bad4e057fcb
EugeneBalievsky <111220873+EugeneBalievsky@users.noreply.github.com>
2022-08-27 02:21:55 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -12,17 +12,19 @@ const int HEIGHT = 1000;
 
 
 int main() {
-    int a = 24;
-    int b = 24;
-    int c = 1;
-    int d = 0;
-    int m = 1;
+    int second_bound = 51;
+    int first_bound = 1;
+    int x = 24;
+    int y = 24;
+    int dx = 1;
+    int dy = 0;
+    int s_size = 1;
     int de = 300;
 
     std::vector<std::vector<field_element>> vec(50, std::vector<field_element>(50));
     vec = create_field();
     std::vector <std::pair <int, int>> v_snake;
-    v_snake = create_snake(b, a, m);
+    v_snake = create_snake(y, x, s_size);
   
    
     initwindow(WIDTH, HEIGHT, "SnakeGame", 0, 0, true);
@@ -30,8 +32,8 @@ int main() {
     
     //создает еду в рандомной ячейке
     srand(time(0));
-    int food_y = rand() % 49;
-    int food_x = rand() % 49;
+    int food_y = rand() % 49+1;
+    int food_x = rand() % 49+1;
    
     while (true) {
         
@@ -52,43 +54,43 @@ int main() {
                 key = getch();
             }
             if (key == KEY_LEFT) {
-                c = -1;
-                d = 0;
+                dx = -1;
+                dy = 0;
             }
             if (key == KEY_RIGHT) {
-                c = 1;
-                d = 0;
+                dx = 1;
+                dy = 0;
             }
             if (key == KEY_UP) {
-                d = -1;
-                c = 0;
+                dy = -1;
+                dx = 0;
             }
             if (key == KEY_DOWN) {
-                d = 1;
-                c = 0;
+                dy = 1;
+                dx = 0;
             }
         }
      
-        for (int i = 0; i < m; ++i) {
+        for (int i = 0; i < s_size; ++i) {
             draw_snake(vec, v_snake[i].first, v_snake[i].second); // рисует змейку     
         } 
-        a += c;
-        b += d;
-        if (a >= 51 || a < 1 || b >= 51 || b < 1) {
-            c = 0;
-            d = 0;
+        x += dx;
+        y += dy;
+        if (x >= second_bound || x < first_bound || y >= second_bound || y < first_bound) {
+            dx = 0;
+            dy = 0;
             game_over();
         }
 
-        v_snake.push_back({ b, a });
+        v_snake.push_back({ y, x });
         v_snake.erase(v_snake.begin());
     
-        if (a == food_x && b == food_y) {
-            ++m;
+        if (x == food_x && y == food_y) {
+            ++s_size;
             de -= 10;
-            v_snake.resize(m, { food_y, food_x });
-            food_y = rand() % 50;
-            food_x = rand() % 50;
+            v_snake.resize(s_size, { food_y, food_x });
+            food_y = rand() % 49+1;
+            food_x = rand() % 49+1;
         }
       
         swapbuffers();
